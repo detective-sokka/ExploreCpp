54ccccdbc92cb7cb7fbbae8959ca465f93e334f1
detective-sokka <saidutt1997@gmail.com>
2025-09-02 16:09:11 -0400

    Add Simple code to understand move sematics.

--- TrickQuestions/eBayPaymentProcessing.cpp
+++ TrickQuestions/eBayPaymentProcessing.cpp
@@ -1,5 +1,6 @@
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +10,21 @@ bool isValid(const string &txn) {
 
 vector<string> processTransactions(vector<string>&& batch) { 
     vector<string> validated; 
-    for (auto& txn : batch) 
-    { 
-        if (isValid(txn)) 
-            validated.emplace_back(std::move(txn)); 
-    } 
+    cout << "Hello";
+    try {
+        for (auto& txn : batch) 
+            if (isValid(txn)) 
+                validated.emplace_back(std::move(txn)); 
+    }
+    catch (exception e) {
+        cout << endl << e.what();
+    }
+    
+    cout << endl << batch[0];
     return validated; 
+}
+
+int main() {
+    processTransactions(vector<string>{"abc", "def", "ghi"});
+    return 0;
 }
